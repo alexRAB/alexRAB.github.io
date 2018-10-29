4aef9e85f2002873e5cc8da71aa6acb44fe9b4e0
Risteiu Alex <alexristeiu@gmail.com>
2018-10-29 10:40:40 +0200

    update lab4

--- html5/desenare.js
+++ html5/desenare.js
@@ -1,27 +1,47 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.22.1";
-document.getElementById("id_button").addEventListener("click", start);
-function deseaneaza_cerc(unghi, ctx, w , h)
-{
-  ctx.beginPath();
-  //void ctx.arc(x, y, radius, startAngle, endAngle [, anticlockwise]);
-  ctx.arc(w / 2  + 100 * Math.cos(unghi), h / 2 + 100 * Math.sin(unghi), 50 , 0, 2 * Math.PI);
-  ctx.stroke();
-}
-//----------------------------------------------------------------
-function start()
-{
-   var canvas = document.getElementById("id_canvas");
-   var ctx =  canvas.getContext('2d');
-   var unghiStart = 0 ; // in grade
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.0";
+ document.getElementById("id_start_button").addEventListener("click", start);
+ document.getElementById("id_stop_button").addEventListener("click", stop);
 
-   var myWorker = new Worker("calcul_prime.js");
-   myWorker.onmessage = function(e)
-   {
-     document.getElementById("id_prime").innerHTML = e.data;
-   }
-   setInterval(deseaneaza_cerc, 10, unghiStart, ctx, canvas.width , canvas.height);
+ document.getElementById("id_stop_button").disabled = true;
+ document.getElementById("id_start_button").disabled = false;
 
+	var unghi_start = {unghi:0}; // in grade
 
+ //---------------------------------------------
+ function deseneaza_cerc(unghi, context, w, h)
+ {
+	 context.clearRect(0, 0, w, h);
 
-}
-//--------------------------------------------------------------------
+	 context.beginPath();
+	 context.arc(w / 2 + 100 * Math.cos(unghi.unghi * Math.PI / 180),
+				h / 2 + 100 * Math.sin(unghi.unghi * Math.PI / 180),
+				50, 0, 2 * Math.PI);
+	 context.stroke();
+
+	 unghi.unghi++;
+ }
+ //---------------------------------------------
+ function start()
+ {
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+
+	document.getElementById("id_start_button").disabled = true;
+	document.getElementById("id_stop_button").disabled = false;
+
+	var my_worker = new Worker("calcul_prime.js");
+	my_worker.onmessage = function(e){
+		document.getElementById("id_prime").innerHTML = e.data;
+	}
+
+	id_timer = setInterval(deseneaza_cerc, 10, unghi_start, context, canvas.width, canvas.height);
+ }
+  //---------------------------------------------
+ function stop()
+ {
+	document.getElementById("id_start_button").disabled = false;
+	document.getElementById("id_stop_button").disabled = true;
+
+	clearInterval(id_timer);
+ }
+ //---------------------------------------------
