e7f0bf6b8b32121ef950e1afbe36cd6b4dacbf4d
Risteiu Alex <alexristeiu@gmail.com>
2018-11-05 10:00:40 +0200

    update

--- html5/desenare.js
+++ html5/desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.3";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.4";
  document.getElementById("id_start_button").addEventListener("click", start);
  document.getElementById("id_stop_button").addEventListener("click", stop);
 
@@ -6,7 +6,8 @@ document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 
  document.getElementById("id_start_button").disabled = false;
 
 	var unghi_start = {unghi:0}; // in grade
-  var my_worker = null;
+
+ var my_worker = null;
  //---------------------------------------------
  function deseneaza_cerc(unghi, context, w, h)
  {
@@ -28,14 +29,16 @@ document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 
 
 	document.getElementById("id_start_button").disabled = true;
 	document.getElementById("id_stop_button").disabled = false;
-  if (my_worker == null){
-       my_worker = new Worker("calcul_prime.js");//  fir de executie
-	     my_worker.onmessage = function(e){
-		      document.getElementById("id_prime").innerHTML = e.data;
-	        }
-     }
-  else
-     my_worker.postMessage("start");
+
+	if (my_worker == null){
+		my_worker = new Worker("calcul_prime.js");
+		my_worker.onmessage = function(e){
+			document.getElementById("id_prime").innerHTML = e.data;
+		}
+	}
+	else
+		my_worker.postMessage("start");
+
 	id_timer = setInterval(deseneaza_cerc, 10, unghi_start, context, canvas.width, canvas.height);
  }
   //---------------------------------------------
@@ -45,6 +48,6 @@ document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 
 	document.getElementById("id_stop_button").disabled = true;
 
 	clearInterval(id_timer);
-  my_worker.postMessage("stop");
+	my_worker.postMessage("stop");
  }
  //---------------------------------------------
