0f9f007f332f271d938917c688354f6752ae2351
Risteiu Alex <alexristeiu@gmail.com>
2018-11-12 11:36:02 +0200

    snap v1

--- html5/cam.js
+++ html5/cam.js
@@ -1,7 +1,24 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.1";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.2";
 
 document.getElementById("id_button").addEventListener("click", start);
 
+document.getElementById("id_video").addEventListener("touchstart", on_touch);
+
+document.getElementById("id_video").addEventListener("mousedown", on_touch);
+
+
+
+function on_touch(e)
+{
+
+  var canvas =  document.getElementById("id_canvas");
+  var context = canvas.getContext("2d");
+  var video = document.getElementById("id_video");
+
+  context.drawImage(video, 0 , 0);
+
+}
+//---------------------------------------
 function on_success(e)
 {
    document.getElementById("id_video").srcObject = e;
@@ -14,7 +31,7 @@ function on_failure(e)
 //----------------------------------------------
 function start()
 {
-  var c = {audio: true, video: {facingMode: "environment"}};
+  var c = {audio: true, video: {facingMode: "user"}};
   navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
 }
 //-----------------------------------
