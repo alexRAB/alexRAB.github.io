ae8f12a5f667263d6179f293cc21edda87e47fb5
Risteiu Alex <alexristeiu@gmail.com>
2018-11-26 11:16:39 +0200

    update

--- html5/touch.js
+++ html5/touch.js
@@ -8,7 +8,7 @@ function on_touch_start(e)
 {
   for (var i = 0; i < e.changedTouches.length; i++)
   {
-           var context = canvas.getContext("2D");
+           var context = canvas.getContext("2d");
            context.beginPath();
            context.arc(e.changedTouches[i].pageX, e.changedTouches[i].pageY ,10 ,0 ,2 * Math.PI);
            context.stroke();
