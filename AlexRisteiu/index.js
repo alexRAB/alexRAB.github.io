61ded3781dce17005fa0e577d9bb28b49ee090bb
AlexRisteiu <alexristeiu@gmail.com>
2019-06-22 11:45:43 +0300

    Update img

--- AlexRisteiu/index.js
+++ AlexRisteiu/index.js
@@ -15,7 +15,7 @@ function showPosition(position) {
 
     var latlon = position.coords.latitude + "," + position.coords.longitude;
     var keyGoogleApi = "AIzaSyCdGcSBFqiWpAzJY0IfVd8KhFAGmgEuQvU";
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center= "+latlon+"&zoom=14&size=100x100&scale=1&markers=color:blue%7Csize:tiny%7C"+position.coords.latitude+","+position.coords.longitude+"&sensor=false&key="+keyGoogleApi+"";
-    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'  >";    
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center= "+latlon+"&zoom=14&size=400x300&scale=1&markers=color:blue%7Csize:tiny%7C"+position.coords.latitude+","+position.coords.longitude+"&sensor=false&key="+keyGoogleApi+"";
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"' class='img-fluid' >";    
     
 }
