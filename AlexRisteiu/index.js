188643a785b98d56f04275fd4af012e810ff5652
AlexRisteiu <alexristeiu@gmail.com>
2019-06-22 11:14:15 +0300

    Update scale of map img to '1'

--- AlexRisteiu/index.js
+++ AlexRisteiu/index.js
@@ -15,7 +15,7 @@ function showPosition(position) {
 
     var latlon = position.coords.latitude + "," + position.coords.longitude;
     var keyGoogleApi = "AIzaSyCdGcSBFqiWpAzJY0IfVd8KhFAGmgEuQvU";
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center= "+latlon+"&zoom=14&size=280x180&scale=2&markers=color:blue%7Csize:tiny%7C"+position.coords.latitude+","+position.coords.longitude+"&sensor=false&key="+keyGoogleApi+"";
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center= "+latlon+"&zoom=14&size=280x180&scale=1&markers=color:blue%7Csize:tiny%7C"+position.coords.latitude+","+position.coords.longitude+"&sensor=false&key="+keyGoogleApi+"";
     document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'  >";    
     
 }
